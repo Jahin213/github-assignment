dc047b8ec39561f87f25a0b4c5a6439f25c2d44c
Jahin213 <jss438@drexel.edu>
2026-08-09 18:14:25 -0400

    Update something.py
    Add shout function to app.py
    
    Adds a shout() function that reuses greet() and returns it in uppercase.

--- something.py
+++ something.py
@@ -1,5 +1,12 @@
 def greet(name):
     return f"Hello, {name}!"
 
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+def shout(name):
+    return greet(name).upper()
+
 print(greet("Jahin"))
 print(farewell("Jahin"))
+print(shout("Jahin"))
